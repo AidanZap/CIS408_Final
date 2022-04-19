8f0f106c7ab41138fa9b0adcc0c9279168a16631
AidanZap <a.zapotechne@vikes.csuohio.edu>
2022-04-19 14:52:41 -0400

    database methods

--- api/database.js
+++ api/database.js
@@ -34,13 +34,13 @@ const makeQuery = async (queryString) => {
 const createTables = async () => {
     await makeQuery(`CREATE TABLE Ingredients (
         IngredientID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
-        Name nvarchar(255) NOT NULL,
+        Name nvarchar(255) NOT NULL UNIQUE,
         Unit nvarchar(64) NOT NULL,
         Category nvarchar(255) NOT NULL
     )`);
     await makeQuery(`CREATE TABLE Recipes (
         RecipeID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
-        Name nvarchar(255)
+        Name nvarchar(255) UNIQUE
     )`);
     await makeQuery(`CREATE TABLE RecipeIngredients (
         RecipeID int NOT NULL FOREIGN KEY REFERENCES Recipes(RecipeID),
@@ -49,9 +49,31 @@ const createTables = async () => {
     )`);
 }
 
+const getRecipeIngredients = async (recipeID) => {
+    let result = await makeQuery(`SELECT i.Name, ri.Quantity, i.Unit, i.Category FROM Ingredients i
+        JOIN RecipeIngredients ri ON i.IngredientID = ri.IngredientID
+        WHERE ri.RecipeID=${recipeID}`);
+    let ingredients = [];
+    result.recordset.forEach((record) => {
+        ingredients.push({"Name": record.Name, "Quantity": record.Quantity, "Unit": record.Unit, "Category": record.Category})
+    })
+    return ingredients;
+}
+
+const insertRecipeIngredient = async (recipeID, ingredientID, quantity) => {
+    return await makeQuery(`INSERT INTO RecipeIngredients (RecipeID, IngredientID, quantity) 
+        VALUES (${recipeID}, ${ingredientID}, ${quantity})`);
+}
+
+const deleteRecipeIngredient = async (recipeID, ingredientID) => {
+    return await makeQuery(`DELETE FROM RecipeIngredients 
+        WHERE RecipeID = ${recipeID} AND IngredientID = ${ingredientID}`);
+}
+
 const main = async () => {
     await readConfig();
-    await createTables();
+    let result = await getRecipeIngredients(1);
+    process.stdout.write(JSON.stringify(result));
 }
 
 main();
--- api/server.js
+++ api/server.js
@@ -24,6 +24,17 @@ recipeRouter.post("/", (req, res) => {
     res.sendStatus(200);
 })
 
+// Recipe Ingredients
+
+const recipeIngredientRouter = express.Router();
+app.use('/recipeIngredient', recipeIngredientRouter);
+
+recipeIngredientRouter.post("/", (req, res) => {
+    let payload = req.body;
+    process.stdout.write(`POSTING | ${payload}\n`);
+    res.sendStatus(200);
+})
+
 // Init Server
 
 app.listen(PORT, () => {
