3a114333b976eb3b56bd3813b9c91055cd75baec
AidanZap <a.zapotechne@vikes.csuohio.edu>
2022-04-22 15:21:06 -0400

    React skeleton!

---
+++ app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button, Container } from "@mui/material";
+
+function App() {
+  return <Container>
+    <Button variant='contained'>Look at this button</Button>
+  </Container>;
+}
+
+export default App;
