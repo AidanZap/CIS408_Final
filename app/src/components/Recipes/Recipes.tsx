1820575efc5647d58c8f25052bac4d547d0440d4
AidanZap <a.zapotechne@vikes.csuohio.edu>
2022-04-26 16:18:29 -0400

    navbar working!

---
+++ app/src/components/Recipes/Recipes.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface IProps {
+
+}
+
+const Recipes: React.FC<IProps> = (props: IProps) => {
+
+
+    return <>
+        <p>Recipes component</p>
+    </>;
+}
+
+export default Recipes;
